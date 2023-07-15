a3420bc2eabafa6a17719cec93866f5617daff31
HighPrince1 <odomprincewill@gmail.com>
2023-07-15 22:33:24 +0000

    0x01-lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,26 +1,32 @@
+#!/usr/bin/python3
+'''LockBoxes Challenge'''
+
+
 def canUnlockAll(boxes):
-    n = len(boxes)
-    visited = [False] * n  # Keep track of visited boxes
-    visited[0] = True  # Mark the first box as visited
-    stack = [0]  # Initialize the stack with the first box
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-    while stack:
-        box = stack.pop()
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
 
-        for key in boxes[box]:
-            if key < n and not visited[key]:
-                visited[key] = True
-                stack.append(key)
-
-    return all(visited)
-
-
-# Example cases
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))  # True
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))  # True
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))  # False
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
